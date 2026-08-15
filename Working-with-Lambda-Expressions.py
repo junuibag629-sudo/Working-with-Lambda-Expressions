24003ea58e9276015f8dfe04fa8b17353583e73b
junuibag629 <junuibag629@gmail.com>
2026-08-15 15:20:36 +0900

    수정 내용

--- Working-with-Lambda-Expressions.py
+++ Working-with-Lambda-Expressions.py
@@ -1,4 +1,4 @@
 #표준입력:1.jpg 10.png 11.png 2.jpg 3.png
 #표준입력:97.xlsx 98.docx 99.docx 100.xlsx 102.docx
-files = inut().split()
-print()
+files = input().split()
+print(list(map(lambda x: x.split('.')[0].zfill(3) + '.' + x.split('.')[1], files)))
